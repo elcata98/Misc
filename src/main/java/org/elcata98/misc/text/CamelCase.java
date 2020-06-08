c4cb58e976a6c8dc6bf801b7948aed80f3abe133
Francisco Martinez Posadas <elcata98@gmail.com>
2020-06-08 17:20:55 +0900

    added functional programming versions

--- src/main/java/org/elcata98/misc/text/CamelCase.java
+++ src/main/java/org/elcata98/misc/text/CamelCase.java
@@ -3,6 +3,7 @@ package org.elcata98.misc.text;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CamelCase {
 
@@ -20,6 +21,40 @@ public class CamelCase {
             }
             sb.append(c);
         }
+
+        return sb.toString();
+    }
+
+    public static String camelCaseFunctional(String camelCasing) {
+
+        return Optional.ofNullable(camelCasing)
+                .orElse(StringUtils.EMPTY)
+                .chars()
+                .mapToObj(c -> (char) c)
+                .map(String::valueOf)
+                .map(character -> {
+                    if (Character.isUpperCase(character.charAt(0))) {
+                        return StringUtils.SPACE + character;
+                    } else {
+                        return character;
+                    }
+                }).collect(Collectors.joining());
+    }
+
+    public static String camelCaseF2(String camelCasing) {
+
+        StringBuilder sb = new StringBuilder();
+
+        Optional.ofNullable(camelCasing)
+                .orElse(StringUtils.EMPTY)
+                .chars()
+                .mapToObj(c -> (char) c)
+                .forEach(character -> {
+                    if (Character.isUpperCase(character)) {
+                        sb.append(StringUtils.SPACE);
+                    }
+                    sb.append(character);
+                });
 
         return sb.toString();
     }
